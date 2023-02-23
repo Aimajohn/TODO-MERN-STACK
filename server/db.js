aa5546897601804797ef94f047985c12f60eae92
john <aimajoke16@gmail.com>
2023-02-23 17:10:36 -0500

    settings for production

--- server/db.js
+++ server/db.js
@@ -1,10 +1,12 @@
 import {createPool} from 'mysql2/promise'
+import {config} from 'dotenv'
+config()
 
 export const pool = createPool({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '=Ata51-mA&',
-    database: 'tododb'
+    host: process.env.DB_HOST ,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER ,
+    password: process.env.DB_PASSWORD ,
+    database: process.env.DB_NAME 
 })
 
--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 
 import express from 'express'
-import {PORT} from './config.js'
 import {indexRoutes} from './routes/index.routes.js'
 import {todoRoutes} from './routes/todo.routes.js'
 import {dirname, join} from 'path'
 import {fileURLToPath} from 'url'
+import {config} from 'dotenv'
 import cors from 'cors'
+config()
 
+const PORT = process.env.PORT || 4000
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const app = express()
 app.use(cors())
